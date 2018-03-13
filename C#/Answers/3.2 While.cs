9da8a7765b38364e34c673cb09f4494d4ae5100a
Oscar Olsson <happybits@gmail.com>
2018-03-13 15:09:53 +0100

    3.2 och 2.3

---
+++ C#/Answers/3.2 While.cs
@@ -0,0 +1,19 @@
+﻿private static void RepeatNameWithWhile()
+{
+    Console.Write("Enter your name: ");
+    string yourName = Console.ReadLine();
+
+    Console.Write("Times to repeat: ");
+    int repeat = int.Parse(Console.ReadLine());
+
+    int counter = 0;
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine();
+
+    while (counter < repeat)
+    {
+        Console.WriteLine($"Your name is {yourName}");
+        counter++;
+    }
+}
