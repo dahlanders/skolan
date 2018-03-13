8dd84be5bf6f14b5d876d3e2f27786d3eff3a90f
Oscar Olsson <happybits@gmail.com>
2018-03-13 10:04:16 +0100

    Svar på övning 2.1

---
+++ C#/Answers/2.1 Respond to user input.cs
@@ -0,0 +1,24 @@
+private static void WhatsYourName()
+{
+    Console.Write("What is your name? ");
+    string name = Console.ReadLine();
+
+    Console.Write("How old are you? ");
+    string age = Console.ReadLine();
+
+    Console.Write("What is your favorite letter? ");
+    string letter = Console.ReadLine();
+
+    Console.WriteLine();
+    Console.WriteLine("Thank you!");
+    Console.WriteLine();
+
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine("Your name is: " + name);
+    Console.WriteLine("You are " + age + " years old");
+    Console.WriteLine("Your favorite letter is " + letter);
+
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine();
+}
