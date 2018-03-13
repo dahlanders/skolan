b46b2290d6413efa431515dcde93530f22900b44
Oscar Olsson <happybits@gmail.com>
2018-03-13 15:50:17 +0100

    3.3 svar

---
+++ C#/Answers/3.3 For.cs
@@ -0,0 +1,17 @@
+﻿private static void RepeatNameWithFor()
+{
+    Console.Write("Enter your name: ");
+    string yourName = Console.ReadLine();
+
+    Console.Write("Times to repeat: ");
+    int repeat = int.Parse(Console.ReadLine());
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine();
+
+    for (int counter = 0; counter < repeat; counter++)
+    {
+        Console.WriteLine($"Your name is {yourName}");
+    }
+
+}
