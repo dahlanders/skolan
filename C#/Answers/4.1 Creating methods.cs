b0d85ecad93fc0899b4fef11ad73851faec2f4cc
Oscar Olsson <happybits@gmail.com>
2018-03-14 15:22:14 +0100

    4.1 och 4.2

---
+++ C#/Answers/4.1 Creating methods.cs
@@ -0,0 +1,38 @@
+﻿private static void WithMethodsSimple()
+{
+    string response = GetInputFromUser();
+
+    string[] peopleArray = CreateArrayOfPeople(response, ',');
+
+    RespondToUser(peopleArray);
+}
+
+private static string GetInputFromUser()
+{
+    Console.Write("Enter a list of names: ");
+    string answer = Console.ReadLine();
+    return answer;
+}
+
+private static string[] CreateArrayOfPeople(string response, char separator = ',')
+{
+    string[] list = response.Split(new char[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+
+    return list;
+}
+
+private static void RespondToUser(string[] peopleArray)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    if (peopleArray.Length == 0)
+    {
+        Console.WriteLine("No people in the list");
+        return;
+    }
+
+    foreach (string person in peopleArray)
+    {
+        Console.WriteLine($"***SUPER-{person.ToUpper()}***");
+    }
+}
